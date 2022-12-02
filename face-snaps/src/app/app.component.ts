714309c676044d0df1ea59326223507822bf6a59
Yassin Hammami <yas.hammami@gmail.com>
2022-12-02 18:20:56 +0100

    Je viens de voir les ngIf

--- face-snaps/src/app/app.component.ts
+++ face-snaps/src/app/app.component.ts
@@ -9,17 +9,35 @@ import { FaceSnap } from './models/face-snap-models';
 export class AppComponent implements OnInit {
   title = 'face-snaps';
   mySnap! : FaceSnap;
+  myOtherSnap! : FaceSnap;
+  myLastSnap! : FaceSnap
 
 
 
   ngOnInit() {
-  this.mySnap = new FaceSnap (
-    "Le Japon",
-    "Un très beau pays",
-    "https://images.pexels.com/photos/581299/pexels-photo-581299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    new Date(),
-    0,
-    )
+  this.mySnap = {
+    title : "Le Japon",
+    description :"Un très beau pays",
+    imageUrl : "https://images.pexels.com/photos/581299/pexels-photo-581299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    createdDate : new Date(),
+    snaps : 0,
+    location : "Tokyo"
+  };
+  this.myOtherSnap = {
+    title : "Okinawa",
+    description :"Une très belle plage",
+    imageUrl : "https://images.pexels.com/photos/8582260/pexels-photo-8582260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    createdDate : new Date(),
+    snaps : 0,
+    location : "Okinawa"
+  };
+  this.myLastSnap = {
+    title : "Le Mont Fuji",
+    description :"Une très belle montagne",
+    imageUrl : "https://images.pexels.com/photos/3408353/pexels-photo-3408353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    createdDate : new Date(),
+    snaps : 0,
+  }
   }
 }
 // this.title="LE JAPON";
--- face-snaps/src/app/models/face-snap-models.ts
+++ face-snaps/src/app/models/face-snap-models.ts
@@ -1,13 +1,20 @@
 export class FaceSnap {
+title! : string;
+description! : string;
+imageUrl! : string;
+createdDate! : Date;
+snaps! : number;
+location? : string;
 
-
-    constructor (
-        public title : string,
-        public description : string,
-        public imageUrl : string,
-        public createdDate : Date,
-        public snaps : number,) 
-        {
+// Ici c'est une manière de déclaré une classe avec ses propriété sans se casser la tete 
+    // constructor (
+    //     public title : string,
+    //     public description : string,
+    //     public imageUrl : string,
+    //     public createdDate : Date,
+    //     public snaps : number,
+    //     public location? : string,) 
+    //     {
             
-        }
+    //     }
 }
