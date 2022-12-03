98cd0660f41b8d53e4f3c36bf372ba92d381a2f4
Yassin Hammami <yas.hammami@gmail.com>
2022-12-03 11:51:47 +0100

    Je viens de voir les directives

--- face-snaps/src/app/app.component.ts
+++ face-snaps/src/app/app.component.ts
@@ -8,36 +8,62 @@ import { FaceSnap } from './models/face-snap-models';
 })
 export class AppComponent implements OnInit {
   title = 'face-snaps';
-  mySnap! : FaceSnap;
-  myOtherSnap! : FaceSnap;
-  myLastSnap! : FaceSnap
+//  Ici on dit que la propriete face snap a pour type un tableau de face snap ( voir en dessous pour comprendre la suite)
+  faceSnap! : FaceSnap[]
 
-
-
+  //  Ici je déclare le projet on init et j'attribut les elements que contient la propriete faceSnap
+  // Ici c'est un tableau contenant des objets 
   ngOnInit() {
-  this.mySnap = {
+  this.faceSnap= [
+    {
     title : "Le Japon",
     description :"Un très beau pays",
     imageUrl : "https://images.pexels.com/photos/581299/pexels-photo-581299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     createdDate : new Date(),
-    snaps : 0,
+    snaps : 50,
     location : "Tokyo"
-  };
-  this.myOtherSnap = {
+  },
+  {
     title : "Okinawa",
     description :"Une très belle plage",
     imageUrl : "https://images.pexels.com/photos/8582260/pexels-photo-8582260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     createdDate : new Date(),
-    snaps : 0,
+    snaps : 100,
     location : "Okinawa"
-  };
-  this.myLastSnap = {
+  },
+  {
     title : "Le Mont Fuji",
     description :"Une très belle montagne",
     imageUrl : "https://images.pexels.com/photos/3408353/pexels-photo-3408353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     createdDate : new Date(),
-    snaps : 0,
+    snaps : 150,
+  },
+  // J'en ai crée 3 de plus les memes pour comprendre l'exemple
+  {
+    title : "Le Japon",
+    description :"Un très beau pays",
+    imageUrl : "https://images.pexels.com/photos/581299/pexels-photo-581299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    createdDate : new Date(),
+    snaps : 200,
+    location : "Tokyo"
+  },
+  {
+    title : "Okinawa",
+    description :"Une très belle plage",
+    imageUrl : "https://images.pexels.com/photos/8582260/pexels-photo-8582260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    createdDate : new Date(),
+    snaps : 250,
+    location : "Okinawa"
+  },
+  {
+    title : "Le Mont Fuji",
+    description :"Une très belle montagne",
+    imageUrl : "https://images.pexels.com/photos/3408353/pexels-photo-3408353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    createdDate : new Date(),
+    snaps : 300,
   }
+]
+
   }
 }
 // this.title="LE JAPON";
