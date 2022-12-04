b60245d26582d2c25b587588b5c079c3726b4d67
Yassin Hammami <yas.hammami@gmail.com>
2022-12-04 12:39:17 +0100

    Je viens de finir la 3eme partie du cours de OCLRM

--- face-snaps/src/app/app.module.ts
+++ face-snaps/src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {registerLocaleData} from '@angular/common'
+import * as fr from '@angular/common/locales/fr'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,13 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide : LOCALE_ID, useValue : 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    registerLocaleData(fr.default)
+  }
+}
