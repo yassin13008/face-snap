449040c03062d8e43d04a2f29b45a31a8c52b817
Yassin Hammami <yas.hammami@gmail.com>
2022-12-09 13:18:20 +0100

    Deux technique de voyages entre les elements

--- face-snaps/src/app/landing-page/landing-page.component.ts
+++ face-snaps/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  
+  // Deuxieme technique pour voyager avec router link
+
+  constructor (private router : Router) {}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  toFaceSnap() : void {
+    this.router.navigateByUrl('facesnaps')
+  }
+
 
 }
